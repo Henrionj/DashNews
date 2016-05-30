dc29c339e35d2ff106da09e0a067f9398fd44322
murerr <florian.pignard@iut-valence.fr>
2016-05-30 12:20:01 +0200

    Create test Connection BDD pgAdmine

--- src/fr/iutvalence/info/dut/m2107/dashnews/tests/MainTestBDD.java
+++ src/fr/iutvalence/info/dut/m2107/dashnews/tests/MainTestBDD.java
@@ -2,7 +2,9 @@ package fr.iutvalence.info.dut.m2107.dashnews.tests;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Class MainTestBDD.
@@ -17,23 +19,34 @@ public class MainTestBDD {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String nameJDBC = "pgAdmin";
-		String url = "jdbc:postgresql://hostname:port/dbname";
-		String login = "murrer";
-		String password = "murrer";
+		String nameJDBC = "org.postgresql.Driver";
+		String url = "jdbc:postgresql://murerr:5432/murerr"; // TODO port?
+		String login = "murerr";
+		String password = "murerr";
+		String sql = "select * from consultation;";
 
 		try {
 			Class.forName(nameJDBC);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			System.out.println(" erreur");
 		}
 		Connection connection = null;
+		Statement State = null;
 		try {
 			connection = DriverManager.getConnection(url, login, password);
+
+			State = connection.createStatement();
+			ResultSet result = State.executeQuery(sql);
+			System.out.println(result);
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+
+		if (connection != null) {
+			System.out.println("You are connected now!");
+		} else {
+			System.out.println("Failed to make connection!");
 		}
 	}
 }
