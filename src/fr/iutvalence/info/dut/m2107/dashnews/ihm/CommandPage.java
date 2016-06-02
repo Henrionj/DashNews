c012df72781da187dd29395ad830fb3e2a37461e
ilias-stiti-iut <ilias.stiti@iut-valence.fr>
2016-06-02 12:57:56 +0200

    Worked on CommandPage

--- src/fr/iutvalence/info/dut/m2107/dashnews/ihm/CommandPage.java
+++ src/fr/iutvalence/info/dut/m2107/dashnews/ihm/CommandPage.java
@@ -4,9 +4,15 @@
 package fr.iutvalence.info.dut.m2107.dashnews.ihm;
 
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import fr.iutvalence.info.dut.m2107.dashnews.item.InfoCommand;
 import fr.iutvalence.info.dut.m2107.dashnews.item.SocialNetwork;
@@ -31,23 +37,48 @@ public class CommandPage extends Page{
 		layItems();
 	}
 
+	// TODO "Commandes payées:", this text must'n be editable
+	// TODO Try to add 2 JScrollPane for ind1 and ind2 (1 for paid command and one for command to pay for each JInternalFrame
 	
 	public void layItems() {
+		// JInternalFrame for command done
 		JInternalFrame ind1 = new JInternalFrame(items.get(0).getName(), false, true,false,false);
-		JInternalFrame ind2 = new JInternalFrame(items.get(1).getName(), false, true,false,false);
-		JInternalFrame ind3 = new JInternalFrame(items.get(2).getName(), false, true,false,false);
 		ind1.setSize(new Dimension(items.get(0).getLength(),items.get(0).getHeight()));
+		JPanel panel1 = new JPanel(new BorderLayout());
+		JTextArea text1  = new JTextArea();
+		text1.setText("Commandes payées:");
+	    panel1.add(text1);
+	    JScrollPane scrollPane1 = new JScrollPane(panel1);
+		ind1.getContentPane().add(scrollPane1);
 		ind1.setLocation(125,100);
 		ind1.setVisible(true);
-		ind2.setSize(new Dimension(items.get(1).getLength(),items.get(1).getHeight()));
-		ind2.setVisible(true);
-		ind2.setLocation(400,100);
-		ind3.setSize(new Dimension(items.get(2).getLength(),items.get(2).getHeight()));
-		ind3.setVisible(true);
-		ind3.setLocation(0,220);
 		this.add(ind1);
+		
+		// JInternalFrame for command ordered
+		
+		JInternalFrame ind2 = new JInternalFrame(items.get(1).getName(), false, true,false,false);
+		ind2.setSize(new Dimension(items.get(1).getLength(),items.get(1).getHeight()));
+		JPanel panel2 = new JPanel(new BorderLayout());
+		JTextArea text2  = new JTextArea();
+		text2.setText("Commandes payées:");
+	    panel2.add(text2);
+	    JScrollPane scrollPane2 = new JScrollPane(panel2);
+		ind2.getContentPane().add(scrollPane2);
+		ind2.setLocation(400,100);
+		ind2.setVisible(true);
+		
 		this.add(ind2);
-		this.add(ind3);
+		
+		// JInternalFrame for social network
+		
+		JInternalFrame ind3 = new JInternalFrame(items.get(2).getName(), false, true,false,false);
+		ind3.setSize(new Dimension(items.get(2).getLength(),items.get(2).getHeight()));
+		JPanel panel3 = new JPanel(new BorderLayout());
+	    JScrollPane scrollPane3 = new JScrollPane(panel3);
+		ind3.getContentPane().add(scrollPane3);
+		ind3.setLocation(0,220);
+		ind3.setVisible(true);
+		this.add(ind3);	
 		
 	}
 
