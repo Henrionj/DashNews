eb1fd097b0dcbf9709be5df1c0c0c97894930720
florian <florian@Pc-Flo>
2016-06-03 18:17:48 +0200

    Modification methode layItems in class StockPage

--- src/fr/iutvalence/info/dut/m2107/dashnews/ihm/StockPage.java
+++ src/fr/iutvalence/info/dut/m2107/dashnews/ihm/StockPage.java
@@ -3,8 +3,14 @@
  */
 package fr.iutvalence.info.dut.m2107.dashnews.ihm;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import fr.iutvalence.info.dut.m2107.dashnews.item.Memo;
 import fr.iutvalence.info.dut.m2107.dashnews.item.Product;
@@ -30,59 +36,40 @@ public class StockPage extends Page {
 	 */
 	public StockPage(String name, List<Product> stock) {
 		super(name);
-		this.items.add(new Memo(10, 10));
-		this.items.add(new SocialNetwork(10, 10, ""));
+		this.items.add(new Memo(Memo.DEFAULT_LENGTH, Memo.DEFAULT_HEIGHT));
+		this.items.add(new SocialNetwork(SocialNetwork.DEFAULT_LENGTH,
+				SocialNetwork.DEFAULT_HEIGHT, "Réseau social"));
 		this.stock = (ArrayList<Product>) stock;
-		// this.layItems();
+		this.layItems();
 	}
 
 	@Override
 	public void layItems() {
-		/*
-		 * // JInternalFrame for command done JInternalFrame ind1 = new
-		 * JInternalFrame(this.items.get(0).getName(), false, true, false,
-		 * false); ind1.setSize(new Dimension(this.items.get(0).getLength(),
-		 * this.items .get(0).getHeight())); ind1.setLayout(null); JPanel panel1
-		 * = new JPanel(); JLabel label1 = new JLabel();
-		 * label1.setText("Tableau des stock de produit"); panel1.add(label1);
-		 * JScrollPane scrollPane1 = new JScrollPane(panel1);
-		 * scrollPane1.setBounds(0, 0, 275, 160); JPanel panel12 = new JPanel();
-		 * JLabel label12 = new JLabel(); label12.setText("Info liste produit");
-		 * panel12.add(label12); JScrollPane scrollPane12 = new
-		 * JScrollPane(panel12); scrollPane12.setBounds(0, 160, 275, 160);
-		 * ind1.getContentPane().add(scrollPane1);
-		 * ind1.getContentPane().add(scrollPane12); ind1.setLocation(125, 100);
-		 * ind1.setVisible(true); this.add(ind1);
-		 * 
-		 * // JInternalFrame for command ordered
-		 * 
-		 * JInternalFrame ind2 = new JInternalFrame(this.items.get(1).getName(),
-		 * false, true, false, false); ind2.setSize(new
-		 * Dimension(this.items.get(1).getLength(), this.items
-		 * .get(1).getHeight())); ind2.setLayout(null); JPanel panel2 = new
-		 * JPanel(); JLabel label2 = new JLabel();
-		 * label2.setText("Tableau des stock de produit"); panel2.add(label2);
-		 * JScrollPane scrollPane2 = new JScrollPane(panel2);
-		 * scrollPane2.setBounds(0, 0, 275, 160); JPanel panel22 = new JPanel();
-		 * JLabel label22 = new JLabel(); label22.setText("Info liste produit");
-		 * panel22.add(label22); JScrollPane scrollPane22 = new
-		 * JScrollPane(panel22); scrollPane22.setBounds(0, 160, 275, 160);
-		 * ind2.getContentPane().add(scrollPane2);
-		 * ind2.getContentPane().add(scrollPane22); ind2.setLocation(400, 100);
-		 * ind2.setVisible(true);
-		 * 
-		 * this.add(ind2);
-		 * 
-		 * // JInternalFrame for social network
-		 * 
-		 * JInternalFrame ind3 = new JInternalFrame(this.items.get(2).getName(),
-		 * false, true, false, false); ind3.setSize(new
-		 * Dimension(this.items.get(2).getLength(), this.items
-		 * .get(2).getHeight())); JPanel panel3 = new JPanel(new
-		 * BorderLayout()); JScrollPane scrollPane3 = new JScrollPane(panel3);
-		 * ind3.getContentPane().add(scrollPane3); ind3.setLocation(0, 220);
-		 * ind3.setVisible(true); this.add(ind3);
-		 */
+		// JInternalFrame for Memo
+
+		JInternalFrame ind = new JInternalFrame(this.items.get(0).getName(),
+				false, true, false, false);
+		ind.setSize(new Dimension(this.items.get(0).getLength(), this.items
+				.get(0).getHeight()));
+		JPanel panel = new JPanel(new BorderLayout());
+		JScrollPane scrollPane = new JScrollPane(panel);
+		ind.getContentPane().add(scrollPane);
+		ind.setLocation(672, 220);
+		ind.setVisible(true);
+		this.add(ind);
+
+		// JInternalFrame for social network
+
+		JInternalFrame ind2 = new JInternalFrame(this.items.get(1).getName(),
+				false, true, false, false);
+		ind2.setSize(new Dimension(this.items.get(1).getLength(), this.items
+				.get(1).getHeight()));
+		JPanel panel2 = new JPanel(new BorderLayout());
+		JScrollPane scrollPane2 = new JScrollPane(panel2);
+		ind2.getContentPane().add(scrollPane2);
+		ind2.setLocation(0, 220);
+		ind2.setVisible(true);
+		this.add(ind2);
 
 	}
 
